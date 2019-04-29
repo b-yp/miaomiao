b081be7ff5c838d8897a94843937ee2cb6664d6f
一条菜狗 <bypl@vip.qq.com>
2019-04-30 00:24:55 +0800

    完成电影页面

--- src/routers/index.js
+++ src/routers/index.js
@@ -11,9 +11,13 @@ export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
-    { path: '/', redirect: '/movie'},
+
     movieRouter,
     cinemaRouter,
-    mineRouter
+    mineRouter,
+    {
+      path: '/*',
+      redirect: '/movie'
+    }
   ]
 })
